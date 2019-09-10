6c24a625e313fdc1764dd60ea0fcc4d7cd3ab3a6
sungjin <ksjnv@naver.com>
2019-09-11 00:49:03 +0900

    ch2. binary tree build error with qt

---
+++ data_structure2_code/2강_실습코드(array_tree)/binary_tree/tree.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include "tree.h"
+
+int PN[MAX + 1];	/* 부모 노드 정보 저장 배열 */
+int CL[MAX + 1];	/* 좌측 자식 정보 저장 배열 */
+int CR[MAX + 1];	/* 우측 자식 정보 저장 배열 */
+int cnt;
+
+/*----------------------------------------------------------------------
+Function Name 	: createTree() - 이진트리 구성함수
+인수 			: 없음
+반환값 			: 없음
+----------------------------------------------------------------------*/
+//// template
+//void createTree(void)
+//{
+//    int vertex;
+//    int edge;
+//    int i;
+
+//    // TODO
+//}
+
+// ref solution
+void createTree(void)
+{
+    int vertex;
+    int edge;
+    int i;
+
+    // TODO
+    scanf("%d %d", &vertex, &edge);
+    for(i=1; i<=edge ; i++){
+        int p,c;
+        scanf("%d %d", &p, &c);
+
+        if(CL[p]==0){
+            CL[p]=c;
+        }
+        else{
+            CR[p]=c;
+        }
+
+        PN[c] = p;
+    }
+
+}
+
+
+/*--------------------------------------------------------------
+Function Name 	: TR() - 이진트리 순회 함수
+인수 			: n - 순회 노드 번호
+반환값 			: 없음
+--------------------------------------------------------------*/
+//// template
+//void TR(int n)
+//{
+//    // TODO
+//}
+
+// my solution
+void TR(int n)
+{
+    // TODO
+    if(n==0){
+        return;
+    }
+
+    printf("%d",n);
+    TR(CL[n]);
+    TR(CR[n]);
+}
+
+/*-------------------------------------------------------------
+Function Name 	: FND() - 조상노드 검색 함수
+인수 			: n - 조상노드를 검색할 대상노드
+반환값 			: 없음
+-------------------------------------------------------------*/
+void FND(int n)
+{
+    // TODO
+    int c = n;
+    while(PN[c] != 0){
+        c = PN[c];
+        printf("%d ", c);
+    }
+}
