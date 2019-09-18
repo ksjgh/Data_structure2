7938d6c566b75b5fbe03428f41af8c3565de7f56
sungjin <sj.kwon@lge.com>
2019-09-18 15:42:23 +0900

    change hangul folder name to english
    causes error in Qt

--- data_structure2_code/ch2_array_tree/binary_tree/main.cpp
+++ data_structure2_code/ch2_array_tree/binary_tree/main.cpp
@@ -6,11 +6,11 @@ int main(void) {
 
     createTree();
     printf("노드 순회 : ");
-    TR(1);
+//    TR(1);
     printf("\n");
     printf("방문한 노드 수 : %d\n", cnt);
     printf("5의 조상 : ") ;
-    FND(5);
+//    FND(5);
 
     return 0;
 }
