1107a5307419d53c4fb5b9313e67cef2c12bb3b2
sungjin <ksjnv@naver.com>
2019-09-30 01:59:08 +0900

    ch3 bst , in(pre,post) order traverse

--- data_structure2_code/ch3_7_binarySearchTree/bst/main.cpp
+++ data_structure2_code/ch3_7_binarySearchTree/bst/main.cpp
@@ -32,64 +32,64 @@ int main()
 	postorderTraverse(mytree.root); // 실제 첫번째 데이터노드의 주소 전달
 	printf("\n\n");
 
-	// 노드 검색
-	searchData = 3;
-	resp = searchNode(&mytree, searchData);
-	if (resp != NULL) printf("%d 찾음!!\n\n", resp->data);
-	else printf("%d 못찾음!!\n\n", searchData);
+//	// 노드 검색
+//	searchData = 3;
+//	resp = searchNode(&mytree, searchData);
+//	if (resp != NULL) printf("%d 찾음!!\n\n", resp->data);
+//	else printf("%d 못찾음!!\n\n", searchData);
 
-	searchData = 99;
-	resp = searchNode(&mytree, searchData);
-	if (resp != NULL) printf("%d 찾음!!\n\n", resp->data);
-	else printf("%d 못찾음!!\n\n", searchData);
+//	searchData = 99;
+//	resp = searchNode(&mytree, searchData);
+//	if (resp != NULL) printf("%d 찾음!!\n\n", resp->data);
+//	else printf("%d 못찾음!!\n\n", searchData);
 
-	searchData = 19;
-	resp = searchNode(&mytree, searchData);
-	if (resp != NULL) printf("%d 찾음!!\n\n", resp->data);
-	else printf("%d 못찾음!!\n\n", searchData);
+//	searchData = 19;
+//	resp = searchNode(&mytree, searchData);
+//	if (resp != NULL) printf("%d 찾음!!\n\n", resp->data);
+//	else printf("%d 못찾음!!\n\n", searchData);
 
-	searchData = 23;
-	resp = searchNode(&mytree, searchData);
-	if (resp != NULL) printf("%d 찾음!!\n\n", resp->data);
-	else printf("%d 못찾음!!\n\n", searchData);
+//	searchData = 23;
+//	resp = searchNode(&mytree, searchData);
+//	if (resp != NULL) printf("%d 찾음!!\n\n", resp->data);
+//	else printf("%d 못찾음!!\n\n", searchData);
 
-	// 노드 삭제
-	printf("Node 삭제...\n");
-	deleteData = 7;
-	resp=deleteNode(&mytree, deleteData); // 유형1 : 삭제할 노드의 오른쪽 자식이 없는 경우
-	if (resp != NULL)
-		printf("%d 삭제성공!\n", deleteData);
-	else
-		printf("%d 삭제 실패!!\n", deleteData);
+//	// 노드 삭제
+//	printf("Node 삭제...\n");
+//	deleteData = 7;
+//	resp=deleteNode(&mytree, deleteData); // 유형1 : 삭제할 노드의 오른쪽 자식이 없는 경우
+//	if (resp != NULL)
+//		printf("%d 삭제성공!\n", deleteData);
+//	else
+//		printf("%d 삭제 실패!!\n", deleteData);
 
-	deleteData = 18;
-	resp = deleteNode(&mytree, deleteData); // 유형2 : 삭제할 노드의 오른쪽 자식의 왼쪽 자식이 없는 경우
-	if (resp != NULL)
-		printf("%d 삭제성공!\n", deleteData);
-	else
-		printf("%d 삭제 실패!!\n", deleteData);
+//	deleteData = 18;
+//	resp = deleteNode(&mytree, deleteData); // 유형2 : 삭제할 노드의 오른쪽 자식의 왼쪽 자식이 없는 경우
+//	if (resp != NULL)
+//		printf("%d 삭제성공!\n", deleteData);
+//	else
+//		printf("%d 삭제 실패!!\n", deleteData);
 
-	deleteData = 123;
-	resp = deleteNode(&mytree, deleteData); // 유형3 : 그외 모든 경우
-	if (resp != NULL)
-		printf("%d 삭제성공!\n", deleteData);
-	else
-		printf("%d 삭제 실패!!\n", deleteData);
+//	deleteData = 123;
+//	resp = deleteNode(&mytree, deleteData); // 유형3 : 그외 모든 경우
+//	if (resp != NULL)
+//		printf("%d 삭제성공!\n", deleteData);
+//	else
+//		printf("%d 삭제 실패!!\n", deleteData);
 
-	deleteData = 14;
-	resp = deleteNode(&mytree, deleteData); // 유형3 : 그외 모든 경우
-	if (resp != NULL)
-		printf("%d 삭제성공!\n", deleteData);
-	else
-		printf("%d 삭제 실패!!\n", deleteData);
+//	deleteData = 14;
+//	resp = deleteNode(&mytree, deleteData); // 유형3 : 그외 모든 경우
+//	if (resp != NULL)
+//		printf("%d 삭제성공!\n", deleteData);
+//	else
+//		printf("%d 삭제 실패!!\n", deleteData);
 
-	printf("\n삭제후의 tree내의 자료 출력 : ");
-	inorderTraverse(mytree.root);  
-	printf("\n\n");
+//	printf("\n삭제후의 tree내의 자료 출력 : ");
+//	inorderTraverse(mytree.root);
+//	printf("\n\n");
 	
-	// tree 소멸 (전체 모드 삭제)
-	printf("tree 전체 노드 삭제(tree 소멸...)\n");
-	destroyTree(&mytree);
+//	// tree 소멸 (전체 모드 삭제)
+//	printf("tree 전체 노드 삭제(tree 소멸...)\n");
+//	destroyTree(&mytree);
 	
 	return 0;
 }
